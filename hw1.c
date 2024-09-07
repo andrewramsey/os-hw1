7c9460202b008b52fb2dbf1ec95ace47d36334e5
Andrew Ramsey <drenzy10@gmail.com>
2024-09-06 21:35:12 -0500

    Add files via upload

--- hw1.c
+++ hw1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// The multiply function that will multiply the indices of arrays with each other
+// The multiply function that multiplies the indices of arrays with each other
 int multiply(int a, int b) {
     return a * b;
 }
 
 int main() {
     printf("Start program \n");
-    int size;
+    int size;  
     FILE *fptr;
 
     // Ask the user for the size of the arrays
     printf("Input array size: ");
-    scanf("%d", &size); // Takes user input and declares size as it, %d is int and the comma &size puts the int into the address of size
+    scanf("%d", &size); // Takes user input to use for size, %d takes the int and &size puts the int into address of size
 
-    // Creates arrays of ints using pointers and dynamically allocated memory, user input for size is the size of both arrays
+    // Creates arrays of ints using pointers and dynamically allocated memory
     int *array1 = (int *)malloc(size * sizeof(int));
     int *array2 = (int *)malloc(size * sizeof(int));
 
@@ -26,14 +26,14 @@ int main() {
     printf("Size of the array pointer: %zu \n", sizeof(array1)); // %zu is long unsigned int, using %d creates warnings
 
     int i; // initialize i for loops
-    // Takes user input for each array element
+    
+    // Take user input for every element in both arrays
     printf("Input content of array1:\n");
     for (i = 0; i < size; i++) 
     {
-        printf("array1[%d]: ", i);
-        scanf("%d", &array1[i]);
+        printf("array1[%d]: ", i); // Ask for input
+        scanf("%d", &array1[i]);   // Scans into array index
     }
-
     printf("Input content for array2:\n");
     for (i = 0; i < size; i++) 
     {
@@ -48,9 +48,9 @@ int main() {
     // Perform multiplication and write to file
     for (i = 0; i < size; i++) {
         int product = multiply(array1[i], array2[i]);
-        if (product % 2 == 0) 
+        if (product % 2 == 0) // If the remainder of product when divided by 2 is 0, its even
         {
-            fprintf(fptr, "array1[%d] * array2[%d] = %d \n", i, i, product); // fprintf writes to file like printf
+            fprintf(fptr, "array1[%d] * array2[%d] = %d \n", i, i, product); // fprintf writes to file 
             fprintf(fptr, " %d is an even number\n", product);
         } 
         else 
@@ -61,23 +61,24 @@ int main() {
     }
     printf("Multiplication done\n");
 
-    // Close the file after writing
+    // Close file after writing
     fclose(fptr);
 
     printf("Read file\n");
-    // Open file to read and print its content
+    // Open file to read and print 
     fptr = fopen("hw1_output.txt", "r");
 
-    // Print the content of the file
+    // Print content of the file
     char ch;
-    while ((ch = fgetc(fptr)) != EOF) {
-        putchar(ch);
+    while ((ch = fgetc(fptr)) != EOF) // Gets the next character until end of file
+    {
+        putchar(ch);  // Writes each character 
     }
 
     // Close the file after reading
     fclose(fptr);
 
-    // Free dynamically allocated memory
+    // Free memory
     free(array1);
     free(array2);
 
